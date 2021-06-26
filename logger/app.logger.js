59e12a8d94889818a31759bf4148f1ba7ee49ad5
Ahmed Shalaby <ahmedismaiel@live.com>
2021-06-26 15:05:21 +0200

    - modify the logger module. - set log roatation to be around 1 hour file.

--- logger/app.logger.js
+++ logger/app.logger.js
@@ -3,11 +3,11 @@ const log4js_app = require("log4js");
 // conf
 
 const app_conf = require('../config/app.conf.json')
+let date = new Date()
 
-
-// app
+// app - logs rotates every an hour
 log4js_app.configure({
-    appenders: { app: { type: "file", filename: app_conf.log_path } },
+    appenders: { app: { type: "file", filename: `${app_conf.log_path}/${date.getFullYear()}_${date.getMonth()}_${date.getDate()}_${date.getHours()}.log`} },
     categories: { default: { appenders: ["app"], level: app_conf.log_level } }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,23 @@ let { tlv_encoder } = require('../securitylayer/tlv.encoder')
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  logger.log.trace(req.body);
 
-  res.render('index', { title: 'جهاز المخابرات العامه' ,subtitle:"اداره امن المعلومات"});
+
+  res.render('index', { title: "Middleware app" });
 });
 
 
 // post to encrypt
 
 router.post('/encyprtdata', function (req, res, next) {
-  let response =tlv_encoder(req,res,next);
-  console.log(response);
-  res.send(response);
+  try {
+    let response = tlv_encoder(req, res, next);
+    logger.log.info(`${JSON.stringify(req.body)}  - 200 - ok`);
+    res.send(response);
+    
+  } catch (error) {
+    logger.log.info(req.body);
+    logger.log.error(error);
+  }
 });
 module.exports = router;

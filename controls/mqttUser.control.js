6de9dd24d29fa5f16ef7c9bb50b298ac946c6be5
enjy609 <enjyahmed95@gmail.com>
2022-03-10 11:39:04 +0200

    Control Edits

--- controls/mqttUser.control.js
+++ controls/mqttUser.control.js
@@ -1,22 +1,23 @@
 let s_mqttUsermodel = require('../models_crud/mqttUser.model')
-let { create_log } = require('../middleware/logger.middleware');
+//let { create_log } = require('../middleware/logger.middleware');
 var responseList = require('../config/response.code.json')
 let { log } = require('../config/app.conf.json')
+let { create_log } = require('../controls/log.control')
 
 
 
 function getAll_mqttUsers(req, request_key) {
     return new Promise((resolve, reject) => {
-        create_log('list mqtt_user', log.log_level.trace, responseList.trace.check_data_length.message, log.req_type.inbound, request_key, req)
+        create_log('list mqtt_user', log.log_level.trace, responseList.trace.check_data_length.message, request_key, req)
         s_mqttUsermodel.getAll_mqttUsers().then(data => {
             if (!data || data.length === 0) {
-                create_log('list mqtt_user', log.log_level.info, responseList.error.error_no_data.message, log.req_type.inbound, request_key, req)
+                create_log('list mqtt_user', log.log_level.info, responseList.error.error_no_data.message, request_key, req)
             }
             else
-            create_log('list mqtt_user', log.log_level.info, responseList.success.sucess_data.message, log.req_type.inbound, request_key, req)
+            create_log('list mqtt_user', log.log_level.info, responseList.success.sucess_data.message, request_key, req)
             resolve(data)
         }).catch((error) => {
-            create_log('list mqtt_user', log.log_level.error, error.message, log.req_type.inbound, request_key, req)
+            create_log('list mqtt_user', log.log_level.error, error.message, request_key, req)
             reject(error)
         })
 
@@ -25,16 +26,16 @@ function getAll_mqttUsers(req, request_key) {
 
 function create_mqttUsers(req,request_key) {
     return new Promise((resolve, reject) => {
-        create_log("create mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, log.req_type.inbound, request_key, req)
+        create_log("create mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, request_key, req)
         s_mqttUsermodel.create_mqttUsers(req).then(data => {
             if (!data || data.rowCount === 0) {
-                create_log("create mqtt_user", log.log_level.error, responseList.error.error_already_exists.message, log.req_type.inbound, request_key, req)
+                create_log("create mqtt_user", log.log_level.error, responseList.error.error_already_exists.message, request_key, req)
             }
             else
-            create_log("create mqtt_user", log.log_level.info, responseList.success.success_creating_data.message, log.req_type.inbound, request_key, req)
+            create_log("create mqtt_user", log.log_level.info, responseList.success.success_creating_data.message, request_key, req)
             resolve(data)
         }).catch((error) => {
-            create_log("create mqtt_user", log.log_level.error, error.message, log.req_type.inbound, request_key, req)
+            create_log("create mqtt_user", log.log_level.error, error.message, request_key, req)
             reject(error)
         })
 
@@ -44,16 +45,16 @@ function create_mqttUsers(req,request_key) {
 
 function update_mqttUsers(req,request_key) {
     return new Promise((resolve, reject) => {
-        create_log("update mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, log.req_type.inbound, request_key, req)
+        create_log("update mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, request_key, req)
         s_mqttUsermodel.update_mqttUsers(req).then(data => {
             if (!data || data.rowCount === 0) {
-                create_log("update mqtt_user",log.log_level.info, responseList.error.error_no_data.message, log.req_type.inbound, request_key, req)
+                create_log("update mqtt_user",log.log_level.info, responseList.error.error_no_data.message, request_key, req)
             }
             else
-            create_log("update mqtt_user", log.log_level.info, responseList.success.success_creating_data.message, log.req_type.inbound, request_key, req)
+            create_log("update mqtt_user", log.log_level.info, responseList.success.success_creating_data.message, request_key, req)
             resolve(data)
         }).catch((error) => {
-            create_log("update mqtt_user", log.log_level.error, error.message, log.req_type.inbound, request_key, req)
+            create_log("update mqtt_user", log.log_level.error, error.message, request_key, req)
             reject(error)
         })
 
@@ -63,16 +64,16 @@ function update_mqttUsers(req,request_key) {
 
 function delete_mqttUsers(req,request_key) {
     return new Promise((resolve, reject) => {
-        create_log("delete mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, log.req_type.inbound, request_key, req)
+        create_log("delete mqtt_user", log.log_level.trace, responseList.trace.executing_query.message, request_key, req)
         s_mqttUsermodel.delete_mqttUsers(req).then(data => {
             if (!data || data.rowCount === 0||data.length===0) {
-                create_log("delete mqtt_user",log.log_level.info, responseList.error.error_no_data.message, log.req_type.inbound, request_key, req)
+                create_log("delete mqtt_user",log.log_level.info, responseList.error.error_no_data.message, request_key, req)
             }
             else
-            create_log("delete mqtt_user", log.log_level.info, responseList.success.success_deleting_data.message, log.req_type.inbound, request_key, req)
+            create_log("delete mqtt_user", log.log_level.info, responseList.success.success_deleting_data.message, request_key, req)
             resolve(data)
         }).catch((error) => {
-            create_log("delete mqtt_user", log.log_level.error, error.message, log.req_type.inbound, request_key, req)
+            create_log("delete mqtt_user", log.log_level.error, error.message, request_key, req)
             reject(error)
         })
 
